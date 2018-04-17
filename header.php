5460e680e2f2af2197b37d0600bac3174834d544
Chris Bishop <domingobishop@gmail.com>
2018-04-17 10:45:17 +0100

    landing template

---
+++ header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Command Paper</title>
+
+	<meta name="description" content="">
+	<meta name="author" content="The National Archives">
+
+	<link rel="stylesheet" id="tna-google-fonts"  href="https://fonts.googleapis.com/css?family=Open+Sans%3A400%2C700%2C400italic%2C700italic%7CBitter&#038;ver=4.4.14" type="text/css" media="all" />
+	<link rel="stylesheet" href="./css/tna-base.css?v=1.0">
+
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="img/favicon.ico">
+	<link rel="icon" type="image/vnd.microsoft.icon" href="img/favicon.ico">
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+<div class="notification-banner clearfix">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="notice">
+					<strong class="title">Beta</strong>
+					<p><strong>This is a new service</strong></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<header>
+	<div class="container">
+		<div class="row" id="header">
+			<div class="col-md-6">
+				<div class="icon icon-logo-tna"></div>
+			</div>
+			<div class="col-md-6 text-right">
+
+			</div>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+
+<main>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Command Papers</h1>
+
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php
+include 'footer.php';
+?>
