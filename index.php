bc113248190e09cf5b5d8f46cb5e3e1283e3ce87
Chris Bishop <domingobishop@gmail.com>
2018-04-17 15:15:15 +0100

    table

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ include 'header.php';
 		<div class="row">
 			<div class="col-md-12">
 				<h1>Command Paper Numbers</h1>
-
+				<?php
+				include 'list.php';
+				?>
 			</div>
 		</div>
 	</div>
---
+++ list.php
@@ -0,0 +1,112 @@
+<div class="content-area">
+	<p>Next available number: <strong>226</strong> <span class="create-new"><a href="#">Create new</a></span></p>
+
+	<div class="table-responsive">
+		<table class="table table-hover">
+			<thead>
+			<tr>
+				<th><a href="#">No.</a></th>
+				<th><a href="#">Title</a></th>
+				<th><a href="#">Dept.</a></th>
+				<th><a href="#">Requester</a></th>
+				<th><a href="#">Date publication due</a></th>
+				<th><a href="#">Date requested</a></th>
+				<th>Parliamentary session</th>
+				<th>Comments</th>
+				<th></th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td>225</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>224</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>223</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>223</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>223</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>223</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			<tr>
+				<td>223</td>
+				<td>Nouveau Matin encore</td>
+				<td>Attorney General's Office</td>
+				<td>snail1966@hotmail.com</td>
+				<td>2018-05-06</td>
+				<td>2018-04-10</td>
+				<td>2018-19</td>
+				<td>Test</td>
+				<td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="clearfix">
+		<ul id="menu-disc">
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li>..</li>
+			<li><a href="#">19</a></li>
+			<li><a href="#">&raquo;</a></li>
+		</ul>
+	</div>
+
+</div>
