f28093f142372851814cffb00646a4b560c7d349
Chris Bishop <domingobishop@gmail.com>
2018-04-17 18:11:06 +0100

    edit form

---
+++ edit.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.php';
+?>
+
+<main>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Command Paper Numbers</h1>
+				<?php
+				include 'form-edit.php';
+				?>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php
+include 'footer.php';
+?>
