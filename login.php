008f1f8dc442c804f54834aeda0c7db8f7716287
Chris Bishop <domingobishop@gmail.com>
2018-04-18 16:02:37 +0100

    typo

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
 <div class="content-area">
-	<p>Signin as a <a href="/?user=tna">TNA user</a></p>
-	<p>Signin as a <a href="/?user=odg">ODG user</a></p>
+	<p>Sign in as a <a href="/?user=tna">TNA user</a></p>
+	<p>Sign in as a <a href="/?user=odg">ODG user</a></p>
 </div>
