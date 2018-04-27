5351f63cf736aad8e9908ca64ba13aefbec8fffb
Chris Bishop <domingobishop@gmail.com>
2018-04-27 15:05:45 +0100

    date picker js

--- footer.php
+++ footer.php
@@ -14,17 +14,7 @@
 </footer>
 
 <script type="text/javascript" src="./js/modernizr-custom.js"></script>
-
-<script>
-	var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-	var isIE = !!window.ActiveXObject;
-	var isIE11 = '-ms-scroll-limit' in document.documentElement.style && '-ms-ime-align' in document.documentElement.style;
-	if( isIE || isIE11 || isSafari ) {
-		$('.picker').datepicker({
-			dateFormat: 'yy-mm-dd'
-		});
-	}
-</script>
+<script type="text/javascript" src="./js/command-papers.js"></script>
 
 </body>
 </html>
