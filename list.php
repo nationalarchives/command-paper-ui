ae25740882099d6dd8d02e6ae069c9792affc654
Chris Bishop <domingobishop@gmail.com>
2018-12-17 16:57:25 +0000

    prefix var

--- list.php
+++ list.php
@@ -23,11 +23,17 @@
 			<tbody>
 			<?php for ($i = 225 ; $i >= 216; $i--) { ?>
 			<tr>
-				<td><strong><?php echo $i ?></strong></td>
+				<td><strong><?php
+						$prefix = 'CP';
+						if ($i % 2 == 0) {
+							$prefix = 'CM';
+						};
+						echo $prefix.$i
+						?></strong></td>
 				<td>Nouveau Matin encore</td>
 				<?php if ( $user == 'tna' ) { ?>
 				<td>Attorney General's Office</td>
-				<td>snail1966@hotmail.com</td>
+				<td>email@hotmail.com</td>
 				<?php } ?>
 				<td>2018-05-06</td>
 				<?php if ( $user == 'tna' ) { ?>
@@ -36,7 +42,7 @@
 				<?php } ?>
 				<td>Test</td>
 				<td>
-					<a href="/?user=<?php echo $user ?>&page=edit&number=<?php echo $i ?>" class="control-button" title="Edit request number <?php echo $i ?>">Edit</a>
+					<a href="/?user=<?php echo $user ?>&page=edit&number=<?php echo $prefix.$i ?>" class="control-button" title="Edit request number <?php echo $i ?>">Edit</a>
 					<?php if ( $user == 'tna' ) { ?>
 					<a href="/?user=<?php echo $user ?>&page=delete&number=<?php echo $i ?>" class="control-button" title="Delete request number <?php echo $i ?>">Delete</a>
 					<?php } ?>
